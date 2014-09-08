391a88e46fbe20a77d6bc8e3c3667f6cb939b2e5
Gene Cho <genec2008@gmail.com>
2014-09-07 21:02:18 -0700

    Start Oauth2 coding
    uncomment build.gradle
    add 3 classes from Example 9 auth
    add annotation to Application.java

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/Application.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/Application.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
+import org.magnum.mobilecloud.video.auth.OAuth2SecurityConfiguration;
 import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +16,7 @@ import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletCon
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -36,6 +38,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 // Any class in this package that is annotated with @Controller is going to be
 // automatically discovered and connected to the DispatcherServlet.
 @ComponentScan
+//We use the @Import annotation to include our OAuth2SecurityConfiguration
+//as part of this configuration so that we can have security and oauth
+//setup by Spring
+@Import(OAuth2SecurityConfiguration.class)
 // Changed since Spring Data Rest will not work in this assignment:
 // public class Application extends RepositoryRestMvcConfiguration {
 public class Application {
